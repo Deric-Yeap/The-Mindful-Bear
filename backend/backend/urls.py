bcac4708bec44f30937f82e76736b0776ebf340e
Poh Hui Min <huimin.poh.2021@scis.smu.edu.sg>
2024-08-03 02:55:37 +0800

    created CUD for Response table

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("api.user.urls")),
     path("api/form/", include("api.form.urls")),
-     path("api/question/", include("api.question.urls")),
+    path("api/question/", include("api.question.urls")),
+    path("api/response/", include("api.response.urls")),
 ]
