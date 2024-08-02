de2881fa5aa448d2b1aa581eea22b36e74737064
Poh Hui Min <huimin.poh.2021@scis.smu.edu.sg>
2024-08-03 01:30:31 +0800

    created CRUD for Form

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("api.user.urls")),
+    path("api/form/", include("api.form.urls")),
 ]
