392dd551252ff230bc6a73ddc048a66803ee010e
FiddleMe <bryenyeoh1@gmail.com>
2024-08-11 23:30:25 +0800

    remove django admin from landmakr and exercise

--- backend/api/exercise/admin.py
+++ backend/api/exercise/admin.py
@@ -1,8 +1,2 @@
-from django.contrib import admin
-from .models import Exercise
-
-class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ["exercise_id", "audio_url", "description"]
 
 # Register your models here.
-admin.site.register(Exercise)
--- backend/api/landmark/admin.py
+++ backend/api/landmark/admin.py
@@ -1,9 +1,3 @@
-from django.contrib import admin
-from .models import Landmark
+
 
 # Register your models here.
-class LandmarkAdmin(admin.ModelAdmin):
-    list_display = ["landmark_id", "x_coordinates", "y_coordinates", "exercise"]
-
-# Register your models here.
-admin.site.register(Landmark)
