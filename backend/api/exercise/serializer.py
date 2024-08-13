fde7b2918c1268ba378f63ccb41913a93f39de5f
FiddleMe <bryenyeoh1@gmail.com>
2024-08-13 13:53:55 +0800

    fix exercise validators

--- backend/api/exercise/serializer.py
+++ backend/api/exercise/serializer.py
@@ -7,17 +7,15 @@ class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = ['exercise_id', 'audio_url', 'description']
-# create serializer to validate empty fields
+
 class ExerciseCreateSerializer(serializers.ModelSerializer):
     audio_url = serializers.URLField(validators=[is_field_empty], required = False)
-    description = serializers.CharField(validators=[is_field_empty], required = True)
+   
     class Meta:
         model = Exercise
         fields = ['exercise_id', 'audio_url', 'description']
 # update serialiser to validate empty fields, with arguments optional
 class ExerciseUpdateSerializer(serializers.ModelSerializer):
-    audio_url = serializers.URLField(validators=[is_field_empty], required = False)
-    description = serializers.CharField(validators=[is_field_empty], required = False)
     class Meta:
         model = Exercise
         fields = ['audio_url', 'description']
