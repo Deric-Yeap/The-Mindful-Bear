97880624cc9dd3e0ad8df3356e9a9da8ffe0a168
FiddleMe <bryenyeoh1@gmail.com>
2024-08-26 02:00:29 +0800

    configured mapbox onto react native. added pitching, zoom, and centering to SGH

--- frontend/app/(map)/maps.jsx
+++ frontend/app/(map)/maps.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { View, StyleSheet, Text, ScrollView } from 'react-native'
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    flex: 1,
-    alignSelf: 'stretch',
-  },
-})
+import { View, ScrollView } from 'react-native'
+import Mapbox from '@rnmapbox/maps'
 
 const Map = () => {
+  Mapbox.setAccessToken(process.env.MAPBOX_PUBLIC_KEY)
   return (
     <SafeAreaView className="h-full">
       <ScrollView>
-        <View>
-          <Text>Hi</Text>
+        <View className="flex-1 justify-center items-center">
+          <View className="h-screen w-full">
+            <Mapbox.MapView
+              className="flex-1"
+              styleURL="mapbox://styles/mapbox/streets-v12"
+              zoomEnabled={true}
+              requestDisallowInterceptTouchEvent={true}
+            >
+              <Mapbox.Camera
+                centerCoordinate={[103.8348, 1.2804]}
+                zoomLevel={17}
+                animationMode={'flyto'}
+                animationDuration={1000}
+                pitch={60}
+                // followUserLocation={true}
+                // UserTrackingMode={true}
+              />
+            </Mapbox.MapView>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
