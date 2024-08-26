8ebcbc5870c1e2b62ccbbedd2037c02822d7eb47
Poh Hui Min <huimin.poh.2021@scis.smu.edu.sg>
2024-08-27 03:14:14 +0800

    fix form layout

--- frontend/app/(admin)/form.jsx
+++ frontend/app/(admin)/form.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import TopBrownSearchBar from '../../components/topBrownSearchBar'; 
+import { Link } from 'expo-router';
 
-
-const User = () => {
+const Form = () => {
   const [userState, setUserState] = useState([
     { name: 'Shaun', key: '1' },
     { name: 'Yoshi', key: '2' },
     { name: 'Mario', key: '3' },
     { name: 'Luigi', key: '4' },
+    { name: 'Hui Min', key: '5' },
   ]);
 
   return (
     <SafeAreaView className="flex-1 bg-optimistic-gray-10">
       <TopBrownSearchBar title="Form Management" />
         
-     {/* New Row with Text and Button */}
-    <View className="flex-row justify-between items-center pt-4 pb-6">
-    <Text className="text-mindful-brown-80 font-bold text-3xl">Form</Text>
-    <TouchableOpacity className="bg-mindful-brown-80 px-4 py-1 rounded-full">
-        <Text className="text-white font-bold text-base">Create Form</Text>
-    </TouchableOpacity>
-    </View>
+      {/* New Row with Text and Button */}
+      <View className="flex-row justify-between items-center pt-4 pb-0 px-4">
+        <Text className="text-mindful-brown-80 font-bold text-3xl">Form</Text>
+        <Link href="/create-form" asChild>
+        <TouchableOpacity className="bg-mindful-brown-80 px-4 py-1 rounded-full">
+          <Text className="text-white font-bold text-base">Create Form</Text>
+        </TouchableOpacity>
+        </Link>
+      </View>
 
       <FlatList
         data={userState}
@@ -34,10 +37,10 @@ const User = () => {
           </View>
         )}
         keyExtractor={item => item.key}
-        contentContainerStyle={{ paddingHorizontal: 16 }} // Optional: Add horizontal padding to the FlatList
+        contentContainerStyle={{ paddingHorizontal: 16 }}
       />
     </SafeAreaView>
   );
 };
 
-export default User;
+export default Form;
