9bca0ec1b2f867c599f4506ac2343458724ec4a2
Deric <deric.yeap1@gmail.com>
2024-08-27 19:18:02 +0800

    Landmark field update
    Added fields landmark_name and landmark_image_url to Landmark backend
    Added error_description log to axios interceptor for debugging
    Fixed color mindfulBrown100 in style.js

--- backend/api/landmark/models.py
+++ backend/api/landmark/models.py
@@ -5,8 +5,11 @@ from api.exercise.models import Exercise
 
 
 # Create your models here.
+
 class Landmark(models.Model):
     landmark_id = models.AutoField(primary_key=True)
+    landmark_name = models.CharField(max_length=255, default='Unnamed Landmark')
+    landmark_image_url = models.URLField(max_length=500, null=True, blank=True)
     x_coordinates = models.DecimalField(max_digits=9, decimal_places=6,default=0)
     y_coordinates = models.DecimalField(max_digits=9, decimal_places=6, default=0)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE,default=None)
