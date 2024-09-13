db5a86d474520913be7af078beddd9aea01e4df1
Poh Hui Min <huimin.poh.2021@scis.smu.edu.sg>
2024-09-13 20:46:34 +0800

    standardise design

--- frontend/app/(admin)/exercisemanagement.jsx
+++ frontend/app/(admin)/exercisemanagement.jsx
@@ -14,7 +14,8 @@ import { router } from 'expo-router' // Import router from expo-router
 import { Link } from 'expo-router'
 import StatusBarComponent from '../../components/darkThemStatusBar'
 import { colors } from '../../common/styles'
-import Loading from '../../components/loading'; 
+import Loading from '../../components/loading';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import the icon 
 
 const ExerciseManagement = () => {
   const [loading, setLoading] = useState(true) // Loading state
@@ -95,10 +96,12 @@ const ExerciseManagement = () => {
               className="flex-row items-center justify-between bg-serenity-green-50 rounded-[15px] py-4 px-4 mb-6"
               onPress={() => handleExercisePress(exercise)}
             >
-              <Text className="text-mindful-brown-10 text-base mr-1">
+              <Text className="text-mindful-brown-10 font-bold text-lg">
                 {exercise.description}
               </Text>
-              <MaterialIcons name="chevron-right" size={24} color="#9BB167" />
+              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                  <Icon name="chevron-right" size={20} color={colors.mindfulBrown10} /> 
+                </View>
             </TouchableOpacity>
           ))}
         </View>
