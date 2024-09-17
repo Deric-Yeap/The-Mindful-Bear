327336860ff16b9c97b7820bd4edf73709495e85
wanqileeeee <wanqi.lee.2021@scis.smu.edu.sg>
2024-09-17 17:07:09 +0800

    fix styling of selection of point on map

--- frontend/app/(admin)/selectLocationMap.jsx
+++ frontend/app/(admin)/selectLocationMap.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
-import { SafeAreaView, View, Text, Button } from 'react-native'
+import { SafeAreaView, View, Text, Button, TextInput } from 'react-native'
 import Mapbox from '@rnmapbox/maps'
 import { useRouter } from 'expo-router';
+import CustomButton from '../../components/customButton';
+
 
 const SelectLocationMap = () => {
   Mapbox.setAccessToken(process.env.MAPBOX_PUBLIC_KEY) // Set your Mapbox token
 
   const [selectedLocation, setSelectedLocation] = useState(null)
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
   const router = useRouter() // Initialize router
 
   // Function to handle map tap and extract coordinates
@@ -16,6 +20,12 @@ const SelectLocationMap = () => {
 
     const [longitude, latitude] = coordinates // Longitude comes first in GeoJSON
     setSelectedLocation({ latitude, longitude })
+    
+    const roundedLatitude = latitude.toFixed(3); // Round to 3 decimal places
+    const roundedLongitude = longitude.toFixed(3); // Round to 3 decimal places
+    setSelectedLocation({ latitude: roundedLatitude.toString(), longitude: roundedLongitude.toString() });
+    setLatitude(roundedLatitude.toString());
+    setLongitude(roundedLongitude.toString());
   }
 
   // Function to handle "confirm location" action
@@ -58,9 +68,32 @@ const SelectLocationMap = () => {
 
       {selectedLocation && (
         <View style={{ padding: 10 }}>
-          <Text>Selected Latitude: {selectedLocation.latitude}</Text>
-          <Text>Selected Longitude: {selectedLocation.longitude}</Text>
-          <Button title="Confirm Location" onPress={confirmLocation} />
+          
+      <View className="p-4 bg-white border-t border-gray-300">
+        <View className="mb-4">
+          <Text className="text-mindful-brown-80 text-lg font-bold">Coordinate-x</Text>
+          <TextInput
+            className="mt-2 text-black rounded-3xl bg-gray-200 px-4 py-2 h-10"
+            placeholder="Coordinate-x"
+            value={latitude}
+            onChangeText={setLatitude}
+          />
+        </View>
+        <View>
+          <Text className="text-mindful-brown-80 text-lg font-bold">Coordinate-y</Text>
+          <TextInput
+            className="mt-2 text-black rounded-3xl bg-gray-200 px-4 py-2 h-10"
+            placeholder="Coordinate-y"
+            value={longitude}
+            onChangeText={setLongitude}
+          />
+        </View>
+      </View>
+      <CustomButton
+            className="absolute bottom-0 left-0 right-0 mb-4 mx-4"
+            handlePress={confirmLocation}
+            title="Confirm Location"
+          />
         </View>
       )}
     </SafeAreaView>
