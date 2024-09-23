6c0ce07c525d19fa234aa0d9f09ea5196576bfbb
Deric <deric.yeap1@gmail.com>
2024-09-24 04:06:05 +0800

    Squash migrations, delete formSession & response

--- backend/api/response/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Response
-
-class ResponseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Response
-        fields = '__all__' 
