ff851fa7621bfb70c087c93bd01fb075dcf89fcb
Poh Hui Min <huimin.poh.2021@scis.smu.edu.sg>
2024-09-27 20:25:27 +0800

    to return the search input of user

--- frontend/components/topBrownSearchBar.jsx
+++ frontend/components/topBrownSearchBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import BackButton from '../components/backButton'; // Ensure this path is correct
@@ -6,18 +6,27 @@ import { useNavigation } from '@react-navigation/native'; // Import useNavigatio
 
 const TopBrownSearchBar = ({ title }) => {
   const navigation = useNavigation(); // Initialize navigation
+  const [searchTerm, setSearchTerm] = useState(''); // useState to track the text input
+
+  const handleSearch = () => {
+    console.log(searchTerm);
+  };
 
   return (
     <View className="bg-mindful-brown-100 p-4 pt-2 rounded-b-[32]">
-      {/* Ensure the title is rendered with a Text component */}
       <BackButton title={title} />
       <View className="flex-row items-center mt-4">
         <TextInput
           placeholder="Search anything..."
           placeholderTextColor="#F7F4F2"
+          value={searchTerm}
+          onChangeText={setSearchTerm} 
           className="flex-1 bg-mindful-brown-70 p-3 rounded-full text-white"
         />
-        <TouchableOpacity className="ml-2 p-3 bg-mindful-brown-80 rounded-full">
+        <TouchableOpacity
+          className="ml-2 p-3 bg-mindful-brown-80 rounded-full"
+          onPress={handleSearch} 
+        >
           <MaterialIcons name="search" size={24} color="#F7F4F2" />
         </TouchableOpacity>
       </View>
