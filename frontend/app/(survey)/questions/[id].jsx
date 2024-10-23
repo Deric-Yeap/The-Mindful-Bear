d9192bd35ebe2f6d6ee205bd429c5a0051c7b3b4
wgqwerty <owg321@gmail.com>
2024-10-24 01:07:13 +0800

    add in icons for likert

--- frontend/app/(survey)/questions/[id].jsx
+++ frontend/app/(survey)/questions/[id].jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Pressable, ScrollView, TextInput } from 'react-native';
-import { useLocalSearchParams, useRouter } from 'expo-router';
-import { getFormQuestions, getFormName } from '../../../api/form';
-import LoadingPage from '../../../components/loading'; 
-import { setFormQuestion } from '../../../api/form';
-import { FontAwesome } from '@expo/vector-icons';
+import React, { useState, useEffect } from 'react'
+import { View, Text, Pressable, ScrollView, TextInput } from 'react-native'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { getFormQuestions, getFormName } from '../../../api/form'
+import LoadingPage from '../../../components/loading'
+import { setFormQuestion } from '../../../api/form'
+import { FontAwesome } from '@expo/vector-icons'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 
 const CustomRadioButton = ({ selected, onPress }) => {
   return (
@@ -16,98 +17,107 @@ const CustomRadioButton = ({ selected, onPress }) => {
     >
       {selected && <View className="h-3 w-3 rounded-full bg-white" />}
     </Pressable>
-  );
-};
+  )
+}
 
 const QuestionPage = () => {
-  const router = useRouter();
-  const { 
-    id, 
+  const router = useRouter()
+  const {
+    id,
     isRedirectedForms,
     selectedLandmarkData,
-    sessionID, 
-    sessionStarted,     
+    sessionID,
+    sessionStarted,
     start,
     isClickTravel,
-    completedForms: initialCompletedForms } = useLocalSearchParams();  
+    completedForms: initialCompletedForms,
+  } = useLocalSearchParams()
   const [completedForms, setCompletedForms] = useState(() => {
     try {
-      return initialCompletedForms ? JSON.parse(initialCompletedForms) : [];
+      return initialCompletedForms ? JSON.parse(initialCompletedForms) : []
     } catch (error) {
-      console.error("Failed to parse initialCompletedForms:", error);
-      return [];
+      console.error('Failed to parse initialCompletedForms:', error)
+      return []
     }
-  });
-  const [sessionData, setSessionData] = useState({});
-  const [formTitle, setFormTitle] = useState('Survey');
-  const [questions, setQuestions] = useState([]);
-  const [answers, setAnswers] = useState({});
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [loading, setLoading] = useState(true);
-  
+  })
+  const [sessionData, setSessionData] = useState({})
+  const [formTitle, setFormTitle] = useState('Survey')
+  const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState({})
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [loading, setLoading] = useState(true)
 
-  let questionsWithOptions = [];
+  let questionsWithOptions = []
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const fetchedQuestions = await getFormQuestions(id);                
-        const sortedQuestions = fetchedQuestions.questions.sort((a, b) => a.order - b.order);
-        setQuestions(sortedQuestions);         
-        setFormTitle(fetchedQuestions.form_name);
-        setLoading(false); 
+        const fetchedQuestions = await getFormQuestions(id)
+        const sortedQuestions = fetchedQuestions.questions.sort(
+          (a, b) => a.order - b.order
+        )
+        setQuestions(sortedQuestions)
+        setFormTitle(fetchedQuestions.form_name)
+        setLoading(false)
       } catch (error) {
-        console.error('Error fetching form data:', error);
-        setLoading(false); 
+        console.error('Error fetching form data:', error)
+        setLoading(false)
       }
-    };
-    fetchData();
-  }, []);
+    }
+    fetchData()
+  }, [])
 
   const handleAnswerChange = (questionID, value) => {
     setAnswers((prev) => ({
       ...prev,
-      [questionID]: value, 
-    }));
-  };
+      [questionID]: value,
+    }))
+  }
 
   const handleNextQuestion = async () => {
     if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setCurrentQuestionIndex(currentQuestionIndex + 1)
     } else {
-        try {                                           
-          await setFormQuestion(sessionID, id, answers);             
-          router.push({
-            pathname: `/questionaire`,
-            params: {               
-              isRedirectedForms: isRedirectedForms,
-              selectedLandmarkData: selectedLandmarkData, 
-              sessionID: sessionID,
-              sessionStarted: true,               
-              start: start,
-              isClickTravel: isClickTravel,
-              completedForms: JSON.stringify(completedForms),              
-            },
-          });
-        } catch (error) {
-          console.error('Error submitting answers:', error);
-        }
+      try {
+        await setFormQuestion(sessionID, id, answers)
+        router.push({
+          pathname: `/questionaire`,
+          params: {
+            isRedirectedForms: isRedirectedForms,
+            selectedLandmarkData: selectedLandmarkData,
+            sessionID: sessionID,
+            sessionStarted: true,
+            start: start,
+            isClickTravel: isClickTravel,
+            completedForms: JSON.stringify(completedForms),
+          },
+        })
+      } catch (error) {
+        console.error('Error submitting answers:', error)
+      }
     }
-  };
+  }
 
-  const currentQuestion = questions[currentQuestionIndex];
-  const isOptionSelected = answers[currentQuestion?.questionID] !== undefined;
+  const currentQuestion = questions[currentQuestionIndex]
+  const isOptionSelected = answers[currentQuestion?.questionID] !== undefined
 
-  
+  const LikertEmoticons = {
+    0: 'emoticon-dead-outline',
+    1: 'emoticon-sad-outline',
+    2: 'emoticon-neutral-outline',
+    3: 'emoticon-happy-outline',
+    4: 'emoticon-excited-outline',
+  }
+
   if (loading) {
-    return <LoadingPage title="Loading questions..." />;
+    return <LoadingPage title="Loading questions..." />
   }
 
   return (
     <ScrollView className="flex-1 p-4 bg-mindful-brown-10 mt-2">
       <View className="mb-4 flex flex-row justify-between items-center">
         <Text className="text-mindful-brown-100 text-xl font-urbanist-bold">
-          {formTitle + " Assessment"}
+          {formTitle + ' Assessment'}
         </Text>
         <Text className="text-mindful-brown-60 text-xl font-urbanist-bold px-2 py-1 rounded-3xl bg-present-red-10">
           {currentQuestionIndex + 1} of {questions.length}
@@ -119,50 +129,78 @@ const QuestionPage = () => {
           <Text className="text-mindful-brown-100 text-xl font-urbanist-extra-bold mb-6">
             {currentQuestion.question}
           </Text>
-          {currentQuestion.optionSet.description === "Likert" || currentQuestion.optionSet.options.length > 0 ? (
+          {currentQuestion.optionSet.description === 'Likert' ||
+          currentQuestion.optionSet.options.length > 0 ? (
             currentQuestion.optionSet.options.map((option) => (
               <Pressable
                 key={option.id}
-                onPress={() => handleAnswerChange(currentQuestion.questionID, option.value)}
+                onPress={() =>
+                  handleAnswerChange(currentQuestion.questionID, option.value)
+                }
                 className={`flex flex-row justify-between items-center p-4 mb-2 rounded-xl bg-white ${
-                  answers[currentQuestion.questionID] === option.value ? 'bg-serenity-green-50' : ''
+                  answers[currentQuestion.questionID] === option.value
+                    ? 'bg-serenity-green-50'
+                    : ''
                 }`}
               >
+                <MaterialCommunityIcons
+                  name={LikertEmoticons[option.value]}
+                  size={30}
+                  color={
+                    answers[currentQuestion.questionID] === option.value
+                      ? 'white'
+                      : 'black'
+                  }
+                />
                 <Text
                   className={`font-urbanist-bold ${
-                    answers[currentQuestion.questionID] === option.value ? 'text-white' : 'text-mindful-brown-100'
+                    answers[currentQuestion.questionID] === option.value
+                      ? 'text-white'
+                      : 'text-mindful-brown-100'
                   }`}
                 >
                   {option.description}
                 </Text>
                 <CustomRadioButton
-                  selected={answers[currentQuestion.questionID] === option.value}
-                  onPress={() => handleAnswerChange(currentQuestion.questionID, option.value)}
+                  selected={
+                    answers[currentQuestion.questionID] === option.value
+                  }
+                  onPress={() =>
+                    handleAnswerChange(currentQuestion.questionID, option.value)
+                  }
                 />
               </Pressable>
             ))
-          ) : currentQuestion.optionSet.description === "Rating" ? (
+          ) : currentQuestion.optionSet.description === 'Rating' ? (
             <View className="flex flex-row justify-between items-center p-4 mx-6">
               {[1, 2, 3, 4, 5].map((rating) => (
                 <Pressable
                   key={rating}
-                  onPress={() => handleAnswerChange(currentQuestion.questionID, rating)}
+                  onPress={() =>
+                    handleAnswerChange(currentQuestion.questionID, rating)
+                  }
                   className="mx-1"
                 >
                   <FontAwesome
                     name="star"
-                    size={40} 
-                    color={answers[currentQuestion.questionID] >= rating ? '#F4C430' : '#E0E0E0'} // Highlight selected stars in gold, others in grey
+                    size={40}
+                    color={
+                      answers[currentQuestion.questionID] >= rating
+                        ? '#F4C430'
+                        : '#E0E0E0'
+                    } // Highlight selected stars in gold, others in grey
                   />
                 </Pressable>
               ))}
-              </View>
+            </View>
           ) : (
             <TextInput
               className="bg-white p-4 m-2 border border-gray-300 rounded-md text-base"
               placeholder="Type your response here"
               value={answers[currentQuestion.questionID] || ''}
-              onChangeText={(text) => handleAnswerChange(currentQuestion.questionID, text)}
+              onChangeText={(text) =>
+                handleAnswerChange(currentQuestion.questionID, text)
+              }
               multiline={true}
               style={{
                 minHeight: 40,
@@ -172,30 +210,28 @@ const QuestionPage = () => {
               }}
             />
           )}
-
         </View>
       )}
-
 
       <Pressable
         onPress={handleNextQuestion}
         className={`py-4 px-6 rounded-full mt-8 ${
-          isOptionSelected
-            ? 'bg-mindful-brown-70'
-            : 'bg-optimistic-gray-40' 
+          isOptionSelected ? 'bg-mindful-brown-70' : 'bg-optimistic-gray-40'
         }`}
-        disabled={!isOptionSelected} 
+        disabled={!isOptionSelected}
       >
         <Text
           className={`text-center text-lg font-urbanist-bold ${
             isOptionSelected ? 'text-white' : 'text-optimistic-gray-80'
           }`}
         >
-          {currentQuestionIndex < questions.length - 1 ? 'Continue →' : 'Complete'}
+          {currentQuestionIndex < questions.length - 1
+            ? 'Continue →'
+            : 'Complete'}
         </Text>
       </Pressable>
     </ScrollView>
-  );
-};
+  )
+}
 
-export default QuestionPage;
+export default QuestionPage
