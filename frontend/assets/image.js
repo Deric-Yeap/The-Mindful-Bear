ae1833f889ea4ea577b35081c23767662594a478
FiddleMe <bryenyeoh1@gmail.com>
2024-11-05 02:05:44 +0800

    remove pointsVector since not using

--- frontend/assets/image.js
+++ frontend/assets/image.js
@@ -1,7 +1,6 @@
 const adaptiveIcon = require('./adaptive-icon.png')
 const confirmModal = require('./confirmModalImage.png')
 const landmarkIcon = require('./landmark-icon.json')
-const pointsVector = require('./vector.png')
 const mindfulBear = require('./animatedBear.json')
 
 export { adaptiveIcon, confirmModal, landmarkIcon, mindfulBear }
